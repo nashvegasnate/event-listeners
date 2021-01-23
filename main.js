d4f997c9a054cd4060fbca17ae2145810953861a
Nathan Keith <nashvegasnate@gmail.com>
2021-01-23 10:57:04 -0600

    tested selsectors

--- main.js
+++ main.js
@@ -1,74 +1,8 @@
 console.log("CONNECTED");
 
-const firstObject = {};
+const button = document.getElementById("main-btn");
+console.log(button);
 
-const user = {
-  username: 'biz.markie',
-  password: 'abd123',
-  lovesJavascript: true,
-  favoriteNumber: 42,
-}
-
-// DOT NOTATION
-// get values out of an object
-
-// console.log(user.lovesJavascript);
-//  console.log(user.username);
-
-// BRACKET NOTATION
-// const password = 'password';
-// console.log(user[password]);  
-//output: abd123
-// console.log(user['username']);
-//output: biz.markie
-
-//console.log(user[favoriteNumber]);
-
-// ASSIGNING VALUES
-
-// const newUser = {
-  // isNew: true,
-// }
-
-// newUser.username = 'fresh.prince'; //dot notation
-// newUser['password'] = 'abcd1234'; //bracket notation
-// console.log(newUser); 
-//output: {isNew: true, username: 'fresh.prince', password: "abcd1234"}
-
-/*const newObject = {
-  username: 'Nathan',
-  sayHello: function () {
-    console.log(`${this.username} says Hello!`);
-  }
-}
-
-newObject.sayHello(); //output: Nathan says Hello!
-*/
-//METHODS (FUNCTIONS AS VALUE)
-/*const newObject = {
-  username: 'Nathan',
-  sayHello: function () {
-    console.log(`${this.username} says Hello!`);
-  }
-}
-newObject.username = 'Aja';
-newObject.sayHello(); //output: Aja
-*/
-  
-//ITERATE OVER AN PBJECT 
-
-/*const usertTwo = {
-  username: 'triinity.christiana',
-  password: 'xyz234',
-  lovesJavascript: true,
-  favoriteNumber: 12,
-}
-*/
-/*
-for (let key in userTwo) {
-  console.log(key); // keys
-  console.log(userTwo[key]); //values
-  console.log(userTwo.key); //undefined
-  console.log(userTwo); //values
-} 
-*/
+//const button2 = document.querySelector("#main-btn");
+//console.log(button2);
+//output: null at js 4 and 7  NEED TO FIX***
