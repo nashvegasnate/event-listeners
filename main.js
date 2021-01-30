09239ebbd719f8d4c5baa76f885e76a68ebaeb59
Nathan Keith <nashvegasnate@gmail.com>
2021-01-29 20:40:14 -0600

    added for each loop

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 //  .forEach
 
-const array = [1,2,3,4,5,6,]
+const array = [1,2,3,4,5,6]
+array.forEach((item, i) => {
+  console.log(item, i);
+})
+
+
   
   
-  array.forEach((item, i) => {
-    console.log(item,i);
-  } )
+  // array.forEach((item, i) => {
+  //   console.log(item,i);
+  // } )
